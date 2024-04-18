56b0453f838d19f19db5b5aab4367992bab605fa
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-17 21:15:37 -0700

    initiliazed token_factory

---
+++ src/app/intro.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className='p-8 flex flex-col'>
+        <Image src="/favicon.ico" width={55} height={55} alt="logo for addicted inc"/>
+        <header className='text-[2.22rem] p-6 font-extrabold pl-8'>1 click token creation</header>
+        <span className='font-semibold text-[1.5rem]'>ERC721 ERC20 ERC1155 & 🔓ERC404</span>
+        <span className='text-[1.5rem] text-[#bf7272] p-3'>Learn how to manage your token</span>
+        <span className='text-lg text-pretty text-[#d9d6d6] p-12'>Add a website <span className='text-[#3a43eb]'>click</span> here.</span>
+    </div>
+  )
+}
+
+export default Intro
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Intro from './intro';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+       <body className={`${inter.className} bg-[#313131] text-gray-950 relative dark:bg-[#100f0f] dark:text-[#d6d6d6] `}>
+        <Intro />
+       {children}
+       </body>
     </html>
   );
 }
