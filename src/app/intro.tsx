f7b5a017a8517ce4e5eb1018de8b73a3a857f6e7
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-18 23:13:53 -0700

    notes for form

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -9,12 +9,17 @@ function Intro() {
         <header className='text-[2.22rem] p-6 font-extrabold pl-8'>1 click token creation</header>
         <span className='font-semibold text-[1.5rem]'>ERC721 ERC20 ERC1155 & 🔓ERC404</span>
         <span className='text-[1.5rem] text-[#bf7272] p-3'>Learn how to manage your token</span>
+            {/* add form for website creation */}
+            
         <span className='text-lg text-pretty text-[#d9d6d6] p-12'>Add a website <span className='text-[#3a43eb]'>click</span> here.</span>
         <div className='flex flex-col p-6'>Don't trust &rarr; verify. Trusted avax links and resources
         <Link href="https://snowscan.xyz/" className='text-[#0b77be] p-6'>snowscan</Link>
         <Link href="https://debank.com/" className='text-[#0b77be] p-6'>debank</Link>
+        {/* <div className='flex flex-col p-6'>Trusted blast 👇
+        <Link href="https://pacmoon.io/" className='text-[#0b77be] p-6'>pacmoon</Link> */}
         </div>
-    </div>
+        </div>
+    
   )
 }
 
--- src/app/securityTipsFromCommunity.tsx
+++ src/app/securityTipsFromCommunity.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function SecurityTipsFromCommunity() {
   return (
+    <div>
+      {/* <div className='text-3xl p-3 text-center text-[#36e809]'>Did pacman make it into my website? @pacmoon_ needs to like this POST. <span className='text-[#d5d847]'> $PAC &rarr; the community of blast</span></div> */}
+      {/* <Image src="/pac.png" height={488} width={488} alt="pac"className='text-center'></Image> */}
     <div className='text-3xl pl-3'>Community sourced Tips upvote to highlight 👇</div>
+    
+    </div>
   )
 };
