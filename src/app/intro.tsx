96a9dfc5c7c5227e8971bd731a654870b8909be5
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-19 17:02:41 -0700

    backend for website hosting

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -10,8 +10,8 @@ function Intro() {
         <span className='font-semibold text-[1.5rem]'>ERC721 ERC20 ERC1155 & 🔓ERC404</span>
         <span className='text-[1.5rem] text-[#bf7272] p-3'>Learn how to manage your token</span>
             {/* add form for website creation */}
-            
-        <span className='text-lg text-pretty text-[#d9d6d6] p-12'>Add a website <span className='text-[#3a43eb]'>click</span> here.</span>
+
+        <span className='text-lg text-pretty text-[#d9d6d6] p-12'><span className='text-[#3a43eb]'>Click here</span>to add a website.</span>
         <div className='flex flex-col p-6'>Don't trust &rarr; verify. Trusted avax links and resources
         <Link href="https://snowscan.xyz/" className='text-[#0b77be] p-6'>snowscan</Link>
         <Link href="https://debank.com/" className='text-[#0b77be] p-6'>debank</Link>
