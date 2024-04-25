f951cedb1a04dcab0de188dba329fe9414e7df84
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-25 16:20:45 -0700

    link for the arena

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -5,9 +5,11 @@ export default function Header() {
   return (
    
     <div>
-        <div className=' left-3 top-[13rem]'>Whats trending in the arena
-         <Image src='/stars.png' width={55} height={55} alt='stars_logo' className='absolute left-3 top-[13rem]'/>
+        <div className='absolute left-3 top-[13rem]'>Whats trending in the arena
+         <Image src='/stars.png' width={55} height={55} alt='stars_logo' className='relative left-3 top-[1.3rem]'/>
+         <div className='relative top-3'>Link you frens</div>
          </div>
+
     <div>Sections</div>
     
     <div className='absolute right-3 top-0'>
