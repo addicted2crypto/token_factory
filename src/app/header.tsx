8de2b950f48b32f095d049292d1a89e4fdb82322
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-24 21:26:48 -0700

    stars

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 export default function Header() {
   return (
+   
     <div>
+        <div className=' left-3 top-[13rem]'>Whats trending in the arena
+         <Image src='/stars.png' width={55} height={55} alt='stars_logo' className='absolute left-3 top-[13rem]'/>
+         </div>
     <div>Sections</div>
+    
     <div className='absolute right-3 top-0'>
+        
         <button className='pr-8 pt-4'>Connect Wallet</button>
     </div>
+    
     </div>
   )
 }
