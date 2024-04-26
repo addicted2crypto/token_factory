ca04d490d6c0f39c10d4b0ad6229bde070749e4f
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-26 16:02:32 -0700

    frens

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <div>
         <div className='absolute left-3 top-[13rem]'>Whats trending in the arena
          <Image src='/stars.png' width={55} height={55} alt='stars_logo' className='relative left-3 top-[1.3rem]'/>
-         <div className='relative top-3'>Link you frens</div>
+         <div className='relative top-3'>Link your frens</div>
          </div>
 
     <div>Sections</div>
