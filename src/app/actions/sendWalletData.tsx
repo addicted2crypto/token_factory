95c2ab8d4461b4f280d2ae3f4ffab49c9c04eb0d
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-29 22:43:41 -0700

    sendWalletData created correctly

---
+++ src/app/actions/sendWalletData.tsx
@@ -0,0 +1,27 @@
+"use server";
+
+
+import { useState } from 'react';
+
+
+
+
+
+function SendWalletData() {
+   
+
+    const [loggedInUser, setWallet] = useState(0);
+    loggedInUser;
+  return (
+   <>
+    if(loggedInUser){
+        SendWalletData
+    }else{
+        alert("Please Log in")
+    }
+  </>
+  )
+}
+
+
+export default SendWalletData
