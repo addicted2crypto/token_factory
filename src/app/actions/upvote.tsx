20f499a8f8c82ac36782709555791a8ee9ddeee0
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-02 16:16:01 -0700

    top 25

--- src/app/actions/upvote.tsx
+++ src/app/actions/upvote.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const [upvote, downvote] =  useState(0);
 // upvote will ++ count and downvote will -- 
 // the whole vote section will check which state has the most upvotes to store it at the top
+//use state to manage top 25 stories
 export default function Upvote() {
   return (
     <div className='text-md'>upvote</div>
