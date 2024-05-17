8d40b28128db9d89bf7e4a4f4546875e8abbf696
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-16 20:15:23 -0700

    connect wallet hover

--- src/app/actions/connectWallet.tsx
+++ src/app/actions/connectWallet.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ConnectWallet() {
   return (
-    <button className='pr-8 pt-4'>Connect Wallet</button>
+    <button className='pr-8 pt-4 hover:translate-x-2 hover:translate-y-2  hover:text-[#fff]'>Connect Wallet</button>
   )
 }
