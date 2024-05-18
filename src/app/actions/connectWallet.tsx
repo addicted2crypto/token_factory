8f43481cbe05bb18636bfda6500d1416df869180
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-17 20:12:53 -0700

    better button for wallet connect

--- src/app/actions/connectWallet.tsx
+++ src/app/actions/connectWallet.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function ConnectWallet() {
   return (
-    <button className='pr-8 pt-4 hover:translate-x-2 hover:translate-y-2  hover:text-[#fff]'>Connect Wallet</button>
+    <div className='p-1'>
+      <Button variant='default'>Wallet connect</Button>
+    {/* <button className='justified-center pr-8 pt-2 hover:translate-x-2 hover:translate-y-2'>Connect Wallet</button> */}
+    </div>
   )
 }
