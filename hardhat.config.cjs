4ac6223cebfdbd73d4cae8ccfbb7b6bdadaa7319
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-25 12:53:08 -0700

    compiled

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,17 +1,14 @@
-import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-dotenv.config();
-const { PRIVATE_KEY, API_URL } = process.env;
-if (!PRIVATE_KEY || !API_URL) {
-    throw new Error("Please set your PRIVATE_KEY and SEPOLIA_URL in a .env file");
-}
-const config = {
+require ("@nomicfoundation/hardhat-toolbox");
+require ("dotenv").config();
+
+
+module.exports = {
     solidity: "0.8.24",
     networks: {
         sepolia: {
-            url: API_URL,
-            accounts: [PRIVATE_KEY],
+            url: process.env.API_URL,
+            accounts: [process.env.PRIVATE_KEY]
         },
     },
 };
-export default config;
+
