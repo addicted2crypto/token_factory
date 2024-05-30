2721beccb8b083bb2ebaf85e14f457868c6f4503
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-29 21:10:15 -0700

    Vote lucid react

--- src/app/actions/highlightedvotedtips.tsx
+++ src/app/actions/highlightedvotedtips.tsx
@@ -1,7 +1,17 @@
+import { Vote } from 'lucide-react'
 import React from 'react'
 
 export default function Highlightedvotedtips() {
   return (
-    <div className='text-3xl'>This weeks top voted submissions. Community voted tips</div>
+    <div className='flex-col'>
+      <div className='flex justify-center gap-2'>
+      <Vote className='items-center'/>
+      <Vote className='items-center'/>
+      <Vote className='items-center'/>
+      <Vote className='items-center'/>
+      <Vote className='items-center'/>
+      </div>
+    <div className='text-3xl'>This weeks top voted submissions. Community voted tips<Vote /></div>
+    </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({
         <div className='bg-[#c1d7fe] absolute bottom-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5493ff]'>
         </div>
         <Web3Provider>
-        <Header />
-        <Intro />
+        {/* <Header />
+        <Intro /> */}
         <SecurityTipsFromCommunity />
         {/* <SendWalletData /> */}
         
