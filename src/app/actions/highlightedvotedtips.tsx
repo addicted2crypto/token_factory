00425d78693e2d9fd509f6e2e5f00282885492a0
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-05-29 23:30:12 -0700

    looks and import upgrades

--- src/app/actions/highlightedvotedtips.tsx
+++ src/app/actions/highlightedvotedtips.tsx
@@ -1,17 +1,22 @@
-import { Vote } from 'lucide-react'
+import { Vote, ListChecks, Handshake, GlobeLock } from 'lucide-react'
 import React from 'react'
 
 export default function Highlightedvotedtips() {
   return (
     <div className='flex-col'>
       <div className='flex justify-center gap-2'>
+        <ListChecks />
       <Vote className='items-center'/>
       <Vote className='items-center'/>
       <Vote className='items-center'/>
       <Vote className='items-center'/>
       <Vote className='items-center'/>
+      <ListChecks />
       </div>
-    <div className='text-3xl'>This weeks top voted submissions. Community voted tips<Vote /></div>
+    <div className='text-3xl'>This weeks top voted submissions. Community voted tips</div>
+    <div className='flex justify-center gap-3'>
+    <GlobeLock /><Handshake />
+    </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from './header';
-import Intro from './intro';
+import Intro from './tokenCreateSection';
 import SecurityTipsFromCommunity from './securityTipsFromCommunity';
 import Footer from './footer';
 import { Web3Provider } from './Web3Context';
@@ -33,13 +33,13 @@ export default function RootLayout({
     <html lang="en">
        <body className={`${inter.className} bg-[#000000cd] text-gray-950 relative dark:bg-[#100f0f] dark:text-[#d6d6d6] m-auto text-center`}>
   
-        <div className='bg-[#e7ffe1] absolute bottom-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75] pt-28 dark:bg-[#1d3d15]'>
+        <div className='bg-[#e7ffe1] absolute bottom-[-6rem] -z-10 right-[11rem] h-[111.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75] pt-28 dark:bg-[#1d3d15]'>
         </div>
-        <div className='bg-[#c1d7fe] absolute bottom-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5493ff]'>
+        <div className='bg-[#c1d7fe] absolute bottom-[-1rem] -z-10 left-[-35rem] h-[111.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left[-33rem] lg:left[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5493ff]'>
         </div>
         <Web3Provider>
-        {/* <Header />
-        <Intro /> */}
+        <Header />
+        {/* <Intro /> */}
         <SecurityTipsFromCommunity />
         {/* <SendWalletData /> */}
         
