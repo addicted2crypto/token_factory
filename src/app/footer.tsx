f1113109bfa1c7cc1cdd1bd1d7158377ca7fb9ea
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-06-01 01:30:17 -0700

    copyright message

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='addicted-llc'>  
         <section className='text-[#ffffff87] hover:text-[#fff] p-6 hover: cursor-pointer'>
-        &copy; &reg;2024. All rights reserved. We do not support any token created or highlighted on this or any website. We support all token's as an integral part of decentralization. 
+        &copy; &reg;2024. All rights reserved. We do not support any token or websites talked about or highlighted on this or any website. We support all token's as an integral part of decentralization. 
         Security is our top priority in development.
         </section>
         </div>
