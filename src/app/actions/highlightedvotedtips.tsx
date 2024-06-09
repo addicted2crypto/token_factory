a4cd0a931e5181704fd66da872c0d4ee9ddde8ae
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-06-09 01:13:29 -0700

    ui cleanup

--- src/app/actions/highlightedvotedtips.tsx
+++ src/app/actions/highlightedvotedtips.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Highlightedvotedtips() {
   return (
-    <div className='flex-col'>
-      <div className='flex justify-center gap-2'>
+    <div className='flex-col pt-3'>
+      <div className='flex justify-center gap-3.5'>
         <ListChecks />
       <Vote className='items-center'/>
       <Vote className='items-center'/>
@@ -13,7 +13,29 @@ export default function Highlightedvotedtips() {
       <Vote className='items-center'/>
       <ListChecks />
       </div>
-    <div className='text-3xl'>This weeks top voted submissions. Top 10 community voted tips</div>
+    <div className='text-3xl p-6 text-slate-900'>➡️ Top voted submissions. Community chose tips ⬅️</div>
+    <div className='text-lg pb-3'>
+      <ol>
+        <li>
+          <span className='text-[#d4d2d2] absolute left-[18rem]'>1</span>... dont text and drive
+        </li>
+        <li>
+          <span className='text-[#d4d2d2] absolute left-[18rem]'>2</span>... dont eat yellow snow
+        </li>
+        <li>
+          <span className='text-[#d4d2d2] absolute left-[18rem]'>3</span>... stop following infulencers
+        </li>
+        <li>
+          <span className='text-[#d4d2d2] absolute left-[18rem]'>4</span>... think more
+        </li>
+        <li>
+          <span className='text-[#d4d2d2] absolute left-[18rem]'>5</span>... dont leave everything in the same wallet you click shit with
+        </li>
+        <li>
+         <span className='text-[#d4d2d2] absolute left-[18rem]'>6</span>..
+        </li>
+      </ol>
+    </div>
     <div className='flex justify-center gap-3'>
     <GlobeLock /> <Handshake /> <Cctv />
     </div>
