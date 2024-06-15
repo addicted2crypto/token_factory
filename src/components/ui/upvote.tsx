d8c6e27830162d4e594c4c5856e1bf7e2eff86fe
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-06-15 16:02:32 -0700

    upvote signer null check

--- src/components/ui/upvote.tsx
+++ src/components/ui/upvote.tsx
@@ -13,6 +13,7 @@ const tipsContractAddress = "";
 // upvote will ++ count and downvote will -- 
 // the whole vote section will check which state has the most upvotes to store it at the top
 //use state to manage top 25 stories
+let signer = null;
 export default function Upvote() {
   const [currentAccount, setCurrentAccount] = useState<string | null>(null);
   const[count, setCount] = useState(0);
