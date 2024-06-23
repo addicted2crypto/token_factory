85672cb3acfdbfa2a93124cd36bcf203677d6519
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-06-23 15:10:59 -0700

    deploy js fix for testing

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,23 @@
 import hardhat from "hardhat";
 const { ethers } = hardhat;
+// async function main() {
+//     const TipsContract = await ethers.getContractFactory("refactoredTipsContract");
+//     const tipsContract = await TipsContract.deploy();
+//     const refactoredTipsContract = await refactoredTipsContract.deploy();
+//     await tipsContract.deploymentTransaction()?.wait();
+//     await tipsContract.waitForDeployment();
+//     console.log("Tips deployed to:", tipsContract.getAddress);
+// }
 async function main() {
-    const TipsContract = await ethers.getContractFactory("Tips");
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const balance = await deployer.getBalance();
+    console.log("Account balance:", balance.toString());
+
+    const TipsContract = await ethers.getContractFactory("TipsContract");
     const tipsContract = await TipsContract.deploy();
-    const refactoredTipsContract = await refactoredTipsContract.deploy();
-    await tipsContract.deploymentTransaction()?.wait();
-    await tipsContract.waitForDeployment();
-    console.log("Tips deployed to:", tipsContract.getAddress);
+    console.log("Contract deployed to address:", tipsContract.address);
 }
 main()
     .then(() => process.exit(0))
