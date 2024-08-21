56a6389e719d0edc31cb650c70f8334fc01d2aab
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-08-20 22:31:28 -0700

    hover loading

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -93,7 +93,7 @@ const Header = () => {
          
         </div>
       ) : (
-        <p>Loading top tip...</p>
+        <p className='hover:ease-in-out'>Loading top tip...</p>
       )}
       </div>
        {/* <Separator className='my-4'/> */}
